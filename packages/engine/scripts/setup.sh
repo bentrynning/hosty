4c189cb95532bbaa61d0426bcc69389af3ebd728
bentrynning <bent.rynning@gmail.com>
2024-10-21 21:36:03 +0200

    chore: try new setup structure

---
+++ packages/engine/scripts/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Secure the pod
+source /data/hosty/scripts/secure.sh
+
+# Install docker
+source /data/hosty/scripts/docker.sh
+
+# Start up Hosty
+source /data/hosty/scripts/start.sh
+
